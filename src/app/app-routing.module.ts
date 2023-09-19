6a6ba935404cdfcc70eb068b4a1ad58fa48b832b
kwabenakay <121056440+kayjob@users.noreply.github.com>
2023-09-19 16:43:08 +0000

    Added routing to homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllCountriesComponent } from './all-countries/all-countries.component';
+import { CountryPageComponent } from './country-page/country-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AllCountriesComponent,
+    title: 'Rest countries',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
