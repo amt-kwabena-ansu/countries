52e5578a26768f8f8163f4cee51fca1f64c83c35
kwabenakay <121056440+kayjob@users.noreply.github.com>
2023-09-21 09:23:39 +0000

    Implemented theme switcher in ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,11 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, OnInit, Inject, Renderer2 } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'countries';
   theme: string = localStorage.getItem('theme') || 'light';
-  constructor(
-    @Inject(DOCUMENT) private readonly document: Document,
-    private readonly renderer: Renderer2
-  ) {}
-  ngOnInit(): void {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      localStorage.setItem('theme', 'dark');
-      this.setTheme('dark');
-    } else {
-      localStorage.setItem('theme', 'light');
-      this.setTheme('light');
-    }
-  }
-  private setTheme(theme: string) {
-    theme === 'dark'
-      ? this.renderer.addClass(this.document.body, 'dark')
-      : this.renderer.removeClass(this.document.body, 'dark');
-  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,23 +1,42 @@
 import { DOCUMENT } from '@angular/common';
-import { Component,Inject,Renderer2 } from '@angular/core';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-
-  theme = localStorage.getItem('theme')
+export class HeaderComponent implements OnInit {
+  theme = localStorage.getItem('theme');
 
   constructor(
     @Inject(DOCUMENT) private readonly document: Document,
     private readonly renderer: Renderer2
   ) {}
 
-  toggleTheme(){
-    this.theme === 'dark'
+  ngOnInit(): void {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      localStorage.setItem('theme', 'light');
+      this.setTheme('light');
+    } else {
+      localStorage.setItem('theme', 'light');
+      this.setTheme('light');
+    }
+    console.log(this.theme);
+  }
+
+  private setTheme(theme: string) {
+    theme === 'dark'
       ? this.renderer.addClass(this.document.body, 'dark')
       : this.renderer.removeClass(this.document.body, 'dark');
+    this.theme = theme === 'dark' ? 'dark' : 'light';
+  }
+  public toggleTheme() {
+    this.theme === 'dark' ? this.setTheme('light') : this.setTheme('dark');
+
+    this.theme === 'dark'
+      ? localStorage.setItem('theme', 'dark')
+      : localStorage.setItem('theme', 'light');
+    console.log('Theme change');
   }
 }
